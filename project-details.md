702757c53c76f6de0e64d8465229720e5a9a38a5
ibbra <ibbraba@gmail.com>
2022-03-08 19:43:15 +0100

    Creating Stock Entity + 1st TDD

---
+++ project-details.md
@@ -0,0 +1,21 @@
+# PROJECT DETAILS
+
+
+
+
+
+
+
+### Dependecies: 
+
+Composer: require Http-client makerBundle Doctrine PHPUnit  
+
+
+##The goal is to write any kind of tests before any code and code in function of the error returned by launching the test.
+
+
+### Steps: 
+
+-Create PHP Class StockTest  
+-Set a new memory SQLite DB (env.test DB_URL)
+-Create our first fuction 
---
+++ tests/StockTest.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace App\Tests;
+
+
+use App\Entity\Stock;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class StockTest extends KernelTestCase
+{
+    private $entityManager;
+    private $registry;
+
+    protected function setUp(): void
+    {
+       $kernel =  self::bootKernel();
+
+       DatabasePrimer::prime($kernel);
+
+
+
+
+
+       $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
+    }
+
+
+    public function test_stock_can_be_created_in_db(){
+
+        //SetUP
+
+        //Stock
+        $stock = new Stock();
+
+
+        //symbol
+        $price = 1000;
+        $previousClose = 1100;
+        $stock->setSymbol("AMZN")
+              ->setShortName("Amazon INC")
+              ->setCurrency("USD")
+              ->setExchangeName("Nasdaq")
+              ->setRegion("US")
+              ->setPriceChange($price - $previousClose)
+              ->setPrice($price)
+              ->setPreviousClose($previousClose);
+
+
+        $this->entityManager->persist($stock);
+        $this->entityManager->flush();
+
+
+        $stockRepository = $this->entityManager->getRepository(Stock::class);
+
+        $stockRecord = $stockRepository->findOneBy(["symbol" => "AMZN"]);
+        //dd($stockRecord->getShortName());
+
+        //Assertion (One for each field)
+
+        $this->assertEquals("Amazon INC", $stockRecord->getShortName());
+        $this->assertEquals("USD", $stockRecord->getCurrency());
+        $this->assertEquals("Nasdaq", $stockRecord->getExchangeName());
+        $this->assertEquals("US", $stockRecord->getRegion());
+        $this->assertEquals(-100, $stockRecord->getPriceChange());
+        $this->assertEquals(1000, $stockRecord->getPrice());
+        $this->assertEquals(1100, $stockRecord->getPreviousClose());
+
+    }
+
+}
