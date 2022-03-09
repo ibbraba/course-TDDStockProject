cb56d3c38a8f8dbc8c052b50232a53e7dd6f63de
ibbra <ibbraba@gmail.com>
2022-03-09 19:23:52 +0100

    Injecting HttpCLient component

--- src/Http/YahooFinanceAPIClient.php
+++ src/Http/YahooFinanceAPIClient.php
@@ -4,9 +4,57 @@
 namespace App\Http;
 
 
+use phpDocumentor\Reflection\Types\This;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 class YahooFinanceAPIClient
 {
+    /**
+     * @var HttpClientInterface
+     */
+    private $client;
+
+    private const URL = "https://yfapi.net/v6/finance/quote";
+    private $YahooAPIKEY;
+
+    public function __construct(HttpClientInterface $client, $YahooAPIKEY)
+    {
+        $this->client = $client;
+        $this->YahooAPIKEY = $YahooAPIKEY;
+    }
+
     public function fetchStockProfile($symbol, $region){
+
+        $request = $this->client->request("GET", self::URL, [
+            //Parameters
+            "query" => [
+                "symbols" => $symbol,
+                "region" => $region
+            ],
+            'headers' => [
+/*                'accept' => 'application/json',*/
+                "x-api-key" => $this->YahooAPIKEY
+            ]
+
+
+        ]);
+
+
+        //decodes JSON objects as PHP array, since 7.2.0 used by default if $assoc parameter is null
+        $stockprofile=json_decode($request->getContent())->quoteResponse->result[0];
+
+        dd($stockprofile->symbol);
+
+
+
+
+        $stockProfileAsArray = [];
+
+        return [
+            "statusCode" => 200,
+            "content" => json_encode($stockProfileAsArray)
+        ];
 
     }
 
